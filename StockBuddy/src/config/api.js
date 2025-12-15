33fc8edd029994cb55594022973e38693da1e889
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2025-12-15 19:21:53 +0530

    Update api.js

--- StockBuddy/src/config/api.js
+++ StockBuddy/src/config/api.js
@@ -1,7 +1,7 @@
 export const NEWS_API_KEY = '5d617ab43bec4b4b9529c65c582eabde'; // Replace with your actual NewsAPI key
 export const NEWS_API_BASE_URL = 'https://newsapi.org/v2';
 export const MODEL_BACKEND_URL = 'http://192.168.214.238:5001';
-export const API_URL = 'http://192.168.214.238:5000';
+export const API_URL = 'https://stockbuddymobilebackend.vercel.app/api';
 
 export const NEWS_CATEGORIES = {
   all: {
